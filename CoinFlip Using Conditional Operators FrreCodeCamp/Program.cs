6e70f15cb5b664b8f0a1324dae36590c5e7a29d0
Sarfaraz Qadir <sarfarazqadir594@gmail.com>
2024-12-12 20:16:43 +0500

    Add project files.

---
+++ CoinFlip Using Conditional Operators FrreCodeCamp/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Coin Flip Conditional Operator");
+
+
+Random rnd = new Random();
+int number = rnd.Next(0,2);
+Console.WriteLine((number == 0) ? "Heads" : "Tails"); 
+
+
+
+
+
